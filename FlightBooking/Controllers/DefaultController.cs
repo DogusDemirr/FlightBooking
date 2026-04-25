614c818429b1dc339254ba64adc93d8c7843fe5e
Doğuş Demir <137798100+DogusDemirr@users.noreply.github.com>
2026-04-25 23:58:46 +0300

    Project creation and theme integration.

---
+++ FlightBooking/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Controllers
+{
+    public class DefaultController : Controller
+    {
+        /// <summary>
+        /// Theme Home Page
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
