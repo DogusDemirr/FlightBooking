a9a08831b7356ca785a603b5012b99db74ea3314
Doğuş Demir <137798100+DogusDemirr@users.noreply.github.com>
2026-04-28 16:46:17 +0300

    MongoDb integration

---
+++ FlightBooking/Entities/Flight.cs
@@ -0,0 +1,62 @@
+﻿namespace FlightBooking.Entities
+{
+    public class Flight
+    {
+        /// <summary>
+        /// Flight ID
+        /// </summary>
+        public string FlightId { get; set; }
+        /// <summary>
+        /// FlightNumber
+        /// </summary>
+        public string FlightNumber { get; set; }
+        /// <summary>
+        /// AirlineCode
+        /// </summary>
+        public string AirlineCode { get; set; }
+        /// <summary>
+        /// DepartureAirportCode
+        /// </summary>
+        public string DepartureAirportCode { get; set; }
+        /// <summary>
+        /// ArrivalAirportCode
+        /// </summary>
+        public string ArrivalAirportCode { get; set; }
+        /// <summary>
+        /// ArrivalAirportName
+        /// </summary>
+        public string ArrivalAirportName { get; set; }
+        /// <summary>
+        /// DepartureTime
+        /// </summary>
+        public DateTime DepartureTime { get; set; }
+        /// <summary>
+        /// ArrivalTime
+        /// </summary>
+        public DateTime ArrivalTime { get; set; }
+        /// <summary>
+        /// DurationMinutes
+        /// </summary>
+        public int DurationMinutes { get; set; }
+        /// <summary>
+        /// TotalSeats
+        /// </summary>
+        public int TotalSeats { get; set; }
+        /// <summary>
+        /// AvailableSeats
+        /// </summary>
+        public int AvailableSeats { get; set; }
+        /// <summary>
+        /// BasePrice
+        /// </summary>
+        public decimal BasePrice { get; set; }
+        /// <summary>
+        /// Currency
+        /// </summary>
+        public string Currency { get; set; }
+        /// <summary>
+        /// Status
+        /// </summary>
+        public string Status { get; set; }
+    }
+}
