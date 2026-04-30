85c97996fe44a9d1f278b0249ff5a982117ae9bf
Doğuş Demir <137798100+DogusDemirr@users.noreply.github.com>
2026-04-30 14:26:36 +0300

    FlightService added

---
+++ FlightBooking/Services/FlightServices/IFlightService.cs
@@ -0,0 +1,37 @@
+﻿using FlightBooking.Dtos.FlightDtos;
+
+namespace FlightBooking.Services.FlightServices
+{
+    public interface IFlightService
+    {
+        /// <summary>
+        /// GetAllFlightsAsync
+        /// </summary>
+        /// <returns></returns>
+        Task<List<ResultFlightDto>> GetAllFlightsAsync();
+        /// <summary>
+        /// GetFlightByIdAsync
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<GetFlightByIdDto> GetFlightByIdAsync(string id);
+        /// <summary>
+        /// CreateFlightAsync
+        /// </summary>
+        /// <param name="createFlightDto"></param>
+        /// <returns></returns>
+        Task CreateFlightAsync(CreateFlightDto createFlightDto);
+        /// <summary>
+        /// DeleteFlightAsync
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task DeleteFlightAsync(string id);
+        /// <summary>
+        /// UpdateFlightDto
+        /// </summary>
+        /// <param name="updateFlightDto"></param>
+        /// <returns></returns>
+        Task UpdateFlightAsync(UpdateFlightDto updateFlightDto);
+    }
+}
