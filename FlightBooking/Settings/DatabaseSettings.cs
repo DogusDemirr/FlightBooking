140d8507ef76064389d3c47e9ea8045be70b3c70
Doğuş Demir <137798100+DogusDemirr@users.noreply.github.com>
2026-04-30 13:42:30 +0300

    ConnectionString settings added

---
+++ FlightBooking/Settings/DatabaseSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace FlightBooking.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        /// <summary>
+        /// ConnectionString
+        /// </summary>
+        public string ConnectionString { get; set; }
+        /// <summary>
+        /// DatabaseName
+        /// </summary>
+        public string DatabaseName { get; set; }
+        /// <summary>
+        /// FlightCollectionName
+        /// </summary>
+        public string FlightCollectionName { get; set; }
+    }
+}
