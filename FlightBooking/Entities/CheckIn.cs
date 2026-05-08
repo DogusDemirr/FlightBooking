c967f965f74cf5d300b9bd7c3ce69128451aaae6
Doğuş Demir <137798100+DogusDemirr@users.noreply.github.com>
2026-05-09 00:25:14 +0300

    CheckIn Entity Designer

---
+++ FlightBooking/Entities/CheckIn.cs
@@ -0,0 +1,38 @@
+﻿namespace FlightBooking.Entities
+{
+    public class CheckIn
+    {
+        /// <summary>
+        /// CheckInId
+        /// </summary>
+        public string CheckInId { get; set; }
+        /// <summary>
+        /// PassengerId
+        /// </summary>
+        public string PassengerId { get; set; }
+        /// <summary>
+        /// FlightId
+        /// </summary>
+        public string FlightId { get; set; }
+        /// <summary>
+        /// PnrNumber
+        /// </summary>
+        public string PnrNumber { get; set; }
+        /// <summary>
+        /// CheckInDate
+        /// </summary>
+        public DateTime CheckInDate { get; set; }
+        /// <summary>
+        /// IsCheckedIn
+        /// </summary>
+        public bool IsCheckedIn { get; set; }
+        /// <summary>
+        /// SeatNumber
+        /// </summary>
+        public string SeatNumber { get; set; }
+        /// <summary>
+        /// ExtraTotalPrice
+        /// </summary>
+        public decimal ExtraTotalPrice { get; set; }
+    }
+}
