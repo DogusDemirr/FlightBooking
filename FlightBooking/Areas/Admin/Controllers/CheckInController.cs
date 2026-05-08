766d16f7cc54faf73fb203afce09e15d130dabff
Doğuş Demir <137798100+DogusDemirr@users.noreply.github.com>
2026-05-09 00:38:08 +0300

    CheckIn UI Designer p1

---
+++ FlightBooking/Areas/Admin/Controllers/CheckInController.cs
@@ -0,0 +1,14 @@
+﻿using FlightBooking.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    [Area(ProjectContants.ADMIN)]
+    public class CheckInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
